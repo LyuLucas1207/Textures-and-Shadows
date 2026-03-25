78fa24b867c542b3b2a89aa25a04342b689e1316
Lucas <2993798118@qq.com>
2026-03-24 22:12:23 -0700

    Part1 a) Finished. Refactor A4.js and GLSL shaders for improved readability and functionality. Adjusted formatting for consistency, added comments for clarity, and implemented texture coordinate handling in the fragment shader. Enhanced vertex shader to output light space position and normal transformations.

--- part1/A4.js
+++ part1/A4.js
@@ -22,7 +22,7 @@ const shadowScene = new THREE.Scene();
 var sceneHandler = 3;
 
 // For ShadowMap visual
-const postCam = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+const postCam = new THREE.OrthographicCamera(- 1, 1, 1, - 1, 0, 1);
 const postScene = new THREE.Scene();
 
 
@@ -47,7 +47,8 @@ const hdrToneMappingOptions = {
   ACESFilmic: THREE.ACESFilmicToneMapping
 };
 
-THREE.DefaultLoadingManager.onLoad = function(){
+THREE.DefaultLoadingManager.onLoad = function ()
+{
   pmremGenerator.dispose();
 };
 
@@ -77,7 +78,7 @@ const helmetMetallicAndRoughnessMap = loadTextureForGLTF('./gltf/DamagedHelmet/D
 
 // This material is already defined for you as it's done in A3
 const helmetMaterial = new THREE.MeshStandardMaterial({
-  emissive: new THREE.Color(1,1,1),
+  emissive: new THREE.Color(1, 1, 1),
   metalness: 1.0,
   envMapIntensity: 1.0,
 
@@ -93,21 +94,23 @@ const helmetMaterial = new THREE.MeshStandardMaterial({
 // Delete this after added the IBL.
 // You need to load the HDR background (./images/rathaus_2k.exr) with the EXRLoader
 let ambientLight = new THREE.AmbientLight(0x404040, 10);
-IBLScene.add( ambientLight );
+IBLScene.add(ambientLight);
 
 const damagedHelmetFilePath = './gltf/DamagedHelmet/DamagedHelmet.gltf';
 let damagedHelmetObject;
 
-await loadGLTFAsync([damagedHelmetFilePath], function(models) {
-      damagedHelmetObject = models[0].scene;
-      damagedHelmetObject.position.set(0, -4.0, -10.0);
-      damagedHelmetObject.scale.set(4, 4, 4);
-      damagedHelmetObject.traverse(function(child) {
-        if (child instanceof THREE.Mesh) {
-          child.material = helmetMaterial;
-        }
-      });
-      IBLScene.add(damagedHelmetObject);
+await loadGLTFAsync([damagedHelmetFilePath], function (models)
+{
+  damagedHelmetObject = models[0].scene;
+  damagedHelmetObject.position.set(0, -4.0, -10.0);
+  damagedHelmetObject.scale.set(4, 4, 4);
+  damagedHelmetObject.traverse(function (child)
+  {
+    if (child instanceof THREE.Mesh) {
+      child.material = helmetMaterial;
+    }
+  });
+  IBLScene.add(damagedHelmetObject);
 });
 
 const IBLControls = new OrbitControls(IBLCamera, canvas);
@@ -116,15 +119,16 @@ IBLControls.maxDistance = 300;
 
 // Register IBLCamera to update its projection on window resize
 if (typeof addResizeCallback === 'function') {
-  addResizeCallback(() => {
+  addResizeCallback(() =>
+  {
     IBLCamera.aspect = window.innerWidth / window.innerHeight;
     IBLCamera.updateProjectionMatrix();
   });
 }
 
 const IBLGUI = new dat.GUI();
-IBLGUI.add( IBLParams, 'hdrToneMapping', Object.keys(hdrToneMappingOptions));
-IBLGUI.add( IBLParams, 'exposure', 0, 2, 0.01 );
+IBLGUI.add(IBLParams, 'hdrToneMapping', Object.keys(hdrToneMappingOptions));
+IBLGUI.add(IBLParams, 'exposure', 0, 2, 0.01);
 IBLGUI.open();
 
 // Q1d Replace the light source with the shadow camera, i.e. setup a camera at the light source
@@ -146,7 +150,7 @@ floorNormalTexture.minFilter = THREE.LinearFilter;
 floorNormalTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
 // Load pixel textures
-const shayDColorTexture = new THREE.TextureLoader().load( 'images/Pixel_Model_BaseColor.jpg' );
+const shayDColorTexture = new THREE.TextureLoader().load('images/Pixel_Model_BaseColor.jpg');
 shayDColorTexture.minFilter = THREE.LinearFilter;
 shayDColorTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
@@ -155,15 +159,15 @@ shayDNormalTexture.minFilter = THREE.LinearFilter;
 shayDNormalTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
 // Uniforms
-const cameraPositionUniform = {type: "v3", value: camera.position}; 
-const lightColorUniform = {type: "c", value: new THREE.Vector3(1.0, 1.0, 1.0)};
-const ambientColorUniform = {type: "c", value: new THREE.Vector3(1.0, 1.0, 1.0)};
-const lightDirectionUniform = {type: "v3", value: lightDirection};
-const kAmbientUniform = {type: "f", value: 0.1};
-const kDiffuseUniform = {type: "f", value: 0.8};
-const kSpecularUniform = {type: "f", value: 0.4};
-const shininessUniform = {type: "f", value: 50.0};
-const lightPositionUniform = { type: "v3", value: shadowCam.position};
+const cameraPositionUniform = { type: "v3", value: camera.position };
+const lightColorUniform = { type: "c", value: new THREE.Vector3(1.0, 1.0, 1.0) };
+const ambientColorUniform = { type: "c", value: new THREE.Vector3(1.0, 1.0, 1.0) };
+const lightDirectionUniform = { type: "v3", value: lightDirection };
+const kAmbientUniform = { type: "f", value: 0.1 };
+const kDiffuseUniform = { type: "f", value: 0.8 };
+const kSpecularUniform = { type: "f", value: 0.4 };
+const shininessUniform = { type: "f", value: 50.0 };
+const lightPositionUniform = { type: "v3", value: shadowCam.position };
 const shayDColorTextureUniform = null; // Q2 TODO make texture uniform
 const shayDNormalTextureUniform = null; // Q2 TODO make texture uniform
 
@@ -171,61 +175,67 @@ const shayDNormalTextureUniform = null; // Q2 TODO make texture uniform
 const skyboxCubemap = null;
 
 // Q1c TODO: make uniform for skybox to pass into shaders
-const skyboxCubeMapUniform = null; 
+const skyboxCubeMapUniform = null;
 
 
 // Materials
 const postMaterial = new THREE.ShaderMaterial({
   uniforms: {
-    lightProjMatrix: {type: "m4", value: shadowCam.projectionMatrix},
-    lightViewMatrix: {type: "m4", value: shadowCam.matrixWorldInverse},
-    tDiffuse: {type: "t", value: null},
+    lightProjMatrix: { type: "m4", value: shadowCam.projectionMatrix },
+    lightViewMatrix: { type: "m4", value: shadowCam.matrixWorldInverse },
+    tDiffuse: { type: "t", value: null },
     tDepth: { type: "t", value: null }
   }
 });
 
 // Updated to use lighting effects in shader files
-const floorMaterial = new THREE.ShaderMaterial({ 
+const floorMaterial = new THREE.ShaderMaterial({
   uniforms: {
-    lightProjMatrix: {type: "m4", value: shadowCam.projectionMatrix},
-    lightViewMatrix: {type: "m4", value: shadowCam.matrixWorldInverse},
+    lightProjMatrix: { type: "m4", value: shadowCam.projectionMatrix },
+    lightViewMatrix: { type: "m4", value: shadowCam.matrixWorldInverse },
     lightColor: lightColorUniform,
     ambientColor: ambientColorUniform,
-    
+
     kAmbient: kAmbientUniform,
     kDiffuse: kDiffuseUniform,
     kSpecular: kSpecularUniform,
     shininess: shininessUniform,
-    
+
     cameraPos: cameraPositionUniform,
     lightPosition: lightPositionUniform,
     lightDirection: lightDirectionUniform,
-    
-    colorMap: {type: "t", value: floorColorTexture},
+
+    colorMap: { type: "t", value: floorColorTexture },
     normalMap: { type: "t", value: floorNormalTexture },
-    shadowMap: {type: "t", value: null},
-    textureSize: {type: "float", value: null},
+    shadowMap: { type: "t", value: null },
+    textureSize: { type: "float", value: null },
   }
 });
 
 // Q1a HINT : Pass the uniforms for blinn-phong shading,
 // colorMap, normalMap etc to the shaderMaterial
+//! =============================== a ===============================
 const shayDMaterial = new THREE.ShaderMaterial({
   side: THREE.DoubleSide,
   uniforms: {
+    lightProjMatrix: { type: "m4", value: shadowCam.projectionMatrix }, //! Same as floorMaterial, add projection matrix
+    lightViewMatrix: { type: "m4", value: shadowCam.matrixWorldInverse }, //! Same as floorMaterial, add view matrix
+    lightColor: lightColorUniform, //! Same as floorMaterial, add light color
+    ambientColor: ambientColorUniform, //! Same as floorMaterial, add ambient color
     kAmbient: kAmbientUniform,
     kDiffuse: kDiffuseUniform,
     kSpecular: kSpecularUniform,
     shininess: shininessUniform,
-    
+
     cameraPos: cameraPositionUniform,
     lightPosition: lightPositionUniform,
     lightDirection: lightDirectionUniform,
 
-    // colorMap: TODO:,
-    // normalMap: TODO:
+    colorMap: { type: "t", value: shayDColorTexture }, //! Using the texture for the color map
+    normalMap: { type: "t", value: shayDNormalTexture }, //! Using the texture for the normal map
   }
 });
+//! =============================== a ===============================
 
 // Q1d Get Shay depth info for shadow casting
 // Needed for Shay depth info.
@@ -252,10 +262,11 @@ const shaderFiles = [
   'glsl/render.fs.glsl',
 ];
 
-new SourceLoader().load(shaderFiles, function(shaders) {
+new SourceLoader().load(shaderFiles, function (shaders)
+{
   shayDMaterial.vertexShader = shaders['glsl/shay.vs.glsl'];
   shayDMaterial.fragmentShader = shaders['glsl/shay.fs.glsl'];
-  
+
   envmapMaterial.vertexShader = shaders['glsl/envmap.vs.glsl'];
   envmapMaterial.fragmentShader = shaders['glsl/envmap.fs.glsl'];
 
@@ -274,11 +285,13 @@ new SourceLoader().load(shaderFiles, function(shaders) {
 const gltfFileName = 'gltf/pixel_v4.glb';
 let shayDObject, shayDShadowObject;
 
-await loadGLTFAsync([gltfFileName], function(models) {
+await loadGLTFAsync([gltfFileName], function (models)
+{
   shayDShadowObject = models[0].scene;
   shayDObject = shayDShadowObject.clone();
 
-  shayDShadowObject.traverse(function(child) {
+  shayDShadowObject.traverse(function (child)
+  {
     if (child instanceof THREE.Mesh) {
       child.material = shadowMaterial;
     }
@@ -287,7 +300,8 @@ await loadGLTFAsync([gltfFileName], function(models) {
   shayDShadowObject.position.set(0.0, 0.0, -8.0);
   shadowScene.add(shayDShadowObject);
 
-  shayDObject.traverse(function(child) {
+  shayDObject.traverse(function (child)
+  {
     if (child instanceof THREE.Mesh) {
       child.material = shayDMaterial;
     }
@@ -303,11 +317,13 @@ terrain.position.y = -2.4;
 terrain.rotation.set(- Math.PI / 2, 0, 0);
 scene.add(terrain);
 
-await loadOBJAsync([ 'gltf/armadillo.obj' ], function(models) {
+await loadOBJAsync(['gltf/armadillo.obj'], function (models)
+{
   const armadillo = models[0];
   armadillo.position.set(0.0, 4.0, 6.0);
   armadillo.scale.set(0.075, 0.075, 0.075);
-  armadillo.traverse(function(child) {
+  armadillo.traverse(function (child)
+  {
     if (child instanceof THREE.Mesh) {
       child.material = envmapMaterial;
     }
@@ -317,11 +333,12 @@ await loadOBJAsync([ 'gltf/armadillo.obj' ], function(models) {
   const armadilloShadow = armadillo.clone();
   armadilloShadow.position.set(0.0, 4.0, 6.0);
   armadilloShadow.scale.set(0.075, 0.075, 0.075);
-  armadilloShadow.traverse(function(child) {
+  armadilloShadow.traverse(function (child)
+  {
     if (child instanceof THREE.Mesh) {
       child.material = shadowMaterial;
     }
-  shadowScene.add(armadilloShadow);
+    shadowScene.add(armadilloShadow);
   });
 });
 
@@ -332,36 +349,37 @@ cube.position.set(0.0, 20.0, -2.0);
 scene.add(cube);
 
 // Depth Test scene
-const postPlane = new THREE.PlaneGeometry( 2, 2 );
-const postQuad = new THREE.Mesh( postPlane, postMaterial );
-postScene.add( postQuad );
+const postPlane = new THREE.PlaneGeometry(2, 2);
+const postQuad = new THREE.Mesh(postPlane, postMaterial);
+postScene.add(postQuad);
 
 
 // Listen to keyboard events.
 const keyboard = new THREEx.KeyboardState();
-function checkKeyboard() {
+function checkKeyboard()
+{
   if (keyboard.pressed("A"))
-  shadowCam.position.x -= 0.2;
+    shadowCam.position.x -= 0.2;
   if (keyboard.pressed("D"))
-  shadowCam.position.x += 0.2;
+    shadowCam.position.x += 0.2;
   if (keyboard.pressed("W"))
-  shadowCam.position.z -= 0.2;
+    shadowCam.position.z -= 0.2;
   if (keyboard.pressed("S"))
-  shadowCam.position.z += 0.2;
+    shadowCam.position.z += 0.2;
   if (keyboard.pressed("Q"))
-  shadowCam.position.y += 0.2;
+    shadowCam.position.y += 0.2;
   if (keyboard.pressed("E"))
-  shadowCam.position.y -= 0.2;
+    shadowCam.position.y -= 0.2;
 
   if (keyboard.pressed("1"))
-  sceneHandler = 1;
+    sceneHandler = 1;
   if (keyboard.pressed("2"))
-  sceneHandler = 2;
+    sceneHandler = 2;
   if (keyboard.pressed("3"))
-  sceneHandler = 3;
+    sceneHandler = 3;
   if (keyboard.pressed("4"))
-  sceneHandler = 4;
-  
+    sceneHandler = 4;
+
   shadowCam.lookAt(scene.position);
   lightDirection.copy(shadowCam.position);
   lightDirection.sub(scene.position);
@@ -369,7 +387,8 @@ function checkKeyboard() {
 
 
 
-function updateMaterials() {
+function updateMaterials()
+{
   envmapMaterial.needsUpdate = true;
   shayDMaterial.needsUpdate = true;
   shadowMaterial.needsUpdate = true;
@@ -378,43 +397,41 @@ function updateMaterials() {
 }
 
 // Setup update callback
-function update() {
+function update()
+{
   checkKeyboard();
   updateMaterials();
 
   cameraPositionUniform.value = camera.position;
-  
+
   requestAnimationFrame(update);
   renderer.getSize(screenSize);
-  renderer.setRenderTarget( null );
+  renderer.setRenderTarget(null);
   renderer.clear();
-  
+
   if (sceneHandler == 1) {
     // Debug, see the scene from the light's perspective
     renderer.render(shadowScene, shadowCam);
   }
-  else if (sceneHandler == 2) 
-  {
+  else if (sceneHandler == 2) {
     // Q1d Visualise the shadow map
     // TODO: First pass to get the depth value
-    
+
     // TODO: Second Pass, visualise shadow map to quad
-    
+
   }
-  else if (sceneHandler == 3) 
-  {
+  else if (sceneHandler == 3) {
     // Q1d Do the multipass shadowing
     // TODO: First pass
-    
-    // TODO: True second pass, change below
-    renderer.setRenderTarget( null );
-    renderer.render( scene, camera );
 
-  } 
-  else 
-  {
+    // TODO: True second pass, change below
+    renderer.setRenderTarget(null);
+    renderer.render(scene, camera);
+
+  }
+  else {
     renderer.physicallyCorrectLights = true;
-    renderer.toneMapping = hdrToneMappingOptions[ IBLParams.hdrToneMapping ];
+    renderer.toneMapping = hdrToneMappingOptions[IBLParams.hdrToneMapping];
     renderer.outputColorSpace = THREE.SRGBColorSpace;
     var prevToneMappingExposure = renderer.toneMappingExposure;
     renderer.toneMappingExposure = IBLParams.exposure;
@@ -428,7 +445,7 @@ function update() {
     renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
     renderer.toneMappingExposure = prevToneMappingExposure;
   }
-   
+
 }
 
 var screenSize = new THREE.Vector2();
